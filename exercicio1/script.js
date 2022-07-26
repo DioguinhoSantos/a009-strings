4f12323efdcd81f3b09d144c11167e7728e6f8c3
DioguinhoSantos <diiogosantos@live.com>
2022-07-25 21:42:07 -0300

    Barbosa-A-Diogo-Santos

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let usuario = prompt("Qual teu nome?");
+let comidaUm = prompt("Das comidas favoritas, a primeira?");
+let comidaDois = prompt("e a segunda?");
+let comidaTres = prompt("e a terceira?");
+
+console.log(`\n\nEstas são as comidas favoritas de ${usuario}:\n - ${comidaUm}\n - ${comidaDois}\n - ${comidaTres}\n\n`);
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
 //Crie a const para a frase aqui
+const frase = `\nJorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"\n`
+
+let fraseDois = frase.replace('verde','rosa');
+let fraseTres = fraseDois.replace('azul','laranja');
+
+console.log(fraseTres);
+
+console.log(`A nova string inclui verde? ${fraseTres.includes('verde')}`);
+console.log(`A nova string inclui laranja? ${fraseTres.includes('laranja')}`);
+
+let fraseCaixaAlta = fraseTres.replace('mas não deixe o gato sair','MAS NÃO DEIXE O GATO SAIR');
+
+console.log(fraseCaixaAlta);
+
